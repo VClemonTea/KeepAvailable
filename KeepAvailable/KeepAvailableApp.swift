07b65c2127df4161bc25878e49d779ea5b7b591d
bennett <bennett.xxbb@outlook.com>
2026-05-27 14:59:46 +0800

    initialize project with SwiftUI template, test targets, and assets

---
+++ KeepAvailable/KeepAvailableApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepAvailableApp.swift
+//  KeepAvailable
+//
+//  Created by Lemoncha on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct KeepAvailableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeepAvailableTests/KeepAvailableTests.swift
@@ -0,0 +1,19 @@
+//
+//  KeepAvailableTests.swift
+//  KeepAvailableTests
+//
+//  Created by Lemoncha on 2026/5/27.
+//
+
+import Testing
+@testable import KeepAvailable
+
+struct KeepAvailableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
